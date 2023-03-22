8ed0ae3e201f0467dad80c6f44521de46af29216
“Naty-S” <na7a5cha010@gmail.com>
2023-03-22 15:43:04 -0400

    structure & layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav.svelte';
+
+</script>
+
+<Nav/>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Billing from "$lib/sections/Billing.svelte";
+  import Business from "$lib/sections/Business.svelte";
+	import CardDeal from "$lib/sections/CardDeal.svelte";
+	import Clients from "$lib/sections/Clients.svelte";
+	import Cta from "$lib/sections/CTA.svelte";
+	import Footer from "$lib/components/Footer.svelte";
+  import Hero from "$lib/sections/Hero.svelte";
+	import Stats from "$lib/sections/Stats.svelte";
+	import Testimonials from "$lib/sections/Testimonials.svelte";
+
+  
+</script>
+
+<div>
+  <Hero/>
+</div>
+
+<div>
+  <Stats/>
+  <Business/>
+  <Billing/>
+  <CardDeal/>
+  <Testimonials/>
+  <Clients/>
+  <Cta/>
+  <Footer/>
+</div>
